1950576cc6d216177acd9b6c424d4a649c4ec9eb
seyed-sabbagh <mohammadjavadsabagh@gmail.com>
2022-07-22 13:46:26 +0430

    Initial commit

---
+++ app/src/main/java/com/arvind/jetcomposeloginui/view/Dashboard.kt
@@ -0,0 +1,94 @@
+package com.arvind.jetcomposeloginui.view
+
+
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.arvind.jetcomposeloginui.R
+import com.arvind.jetcomposeloginui.ui.theme.colorPrimary
+
+@Composable
+fun Dashboard(
+    navController: NavController,
+) {
+    val sectionState = remember { mutableStateOf(DashboardSection.Home) }
+    val navItems = DashboardSection.values().toList()
+
+    Scaffold(
+        bottomBar = {
+            BottomBar(
+                items = navItems,
+                currentSection = sectionState.value,
+                onSectionSelected = { sectionState.value = it },
+            )
+        }) { innerPadding ->
+        val modifier = Modifier.padding(innerPadding)
+        Crossfade(
+            modifier = modifier,
+            targetState = sectionState.value
+        )
+        { section ->
+            when (section) {
+                DashboardSection.Home -> HomeScreen(navController)
+            }
+            when (section) {
+                DashboardSection.ShoppingCart -> CheckoutScreen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    items: List<DashboardSection>,
+    currentSection: DashboardSection,
+    onSectionSelected: (DashboardSection) -> Unit,
+) {
+    BottomNavigation(
+        modifier = Modifier.height(50.dp),
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = contentColorFor(MaterialTheme.colors.background)
+    ) {
+        items.forEach { section ->
+
+            val selected = section == currentSection
+
+            val iconRes = if (selected) section.selectedIcon else section.icon
+
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = iconRes),
+                        modifier = Modifier.size(24.dp),
+                        contentDescription = "Bottom nav icons"
+                    )
+                },
+                selected = selected,
+                unselectedContentColor = Color.Gray,
+                selectedContentColor = colorPrimary,
+                onClick = { onSectionSelected(section) },
+                alwaysShowLabel = false
+            )
+        }
+    }
+}
+
+private enum class DashboardSection(
+    val icon: Int,
+    val selectedIcon: Int,
+) {
+    Home(R.drawable.ic_home, R.drawable.ic_home),
+    List(R.drawable.ic_location, R.drawable.ic_location),
+    ShoppingCart(R.drawable.ic_cart, R.drawable.ic_cart),
+    Profile(R.drawable.ic_profile, R.drawable.ic_profile),
+}
